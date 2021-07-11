2d8c6b27787c2525612c410a9ea0c40f144a6143
thinh <thinhng1012@gmail.com>
2021-07-11 17:03:26 +0700

    Update URL

---
+++ precache-manifest.eeb66458d0d80cf032bdfc7c70b13d35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ab85d54e33465c514ec12a1db44a70",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e78d290b74b88b56fcf6",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98863bde83c432310060",
+    "url": "./static/js/2.71cf1447.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "./static/js/2.71cf1447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e78d290b74b88b56fcf6",
+    "url": "./static/js/main.04f2d01c.chunk.js"
+  },
+  {
+    "revision": "39d890a9758519152bbe",
+    "url": "./static/js/runtime-main.0f4412ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
